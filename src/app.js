5dce9e5ade8e19f079252593c57c600e532561fa
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2024-02-22 05:58:17 +0000

    setGeneradorCard

--- src/app.js
+++ src/app.js
@@ -5,5 +5,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btnCambio").addEventListener("click", () => {
+    document.queryselector;
+  });
+};
+
+const numbersRandom = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+};
+
+const iconsRandom = () => {
+  let icons = ["diamond", "spade", "heart", "club"];
+  let indexIcons = Math.floor(Math.random() * icons.length);
 };
