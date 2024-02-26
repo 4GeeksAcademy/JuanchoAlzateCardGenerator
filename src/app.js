295e9d2ed9eae27216f8f3c36423a9670e6755b7
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2024-02-26 00:19:44 +0000

    setCardGenerator

--- src/app.js
+++ src/app.js
@@ -4,32 +4,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  document.querySelector("#btnCambio").addEventListener("click", () => {
-    document.queryselector;
-  });
+let numbersCard = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let iconCard = ["♠", "♥", "♦", "♣"];
+let contNum = 0;
+let contIcon = 0;
+const inicio = () => {
+  document.querySelector(".btnAleatorio").onclick = cambio;
+  document.querySelector(".btnCambio").onclick = cambioEnOrden;
+};
+const cambioEnOrden = () => {
+  document.querySelector(".numberCard").innerHTML = numbersCard[contNum];
+  if (iconCard[contIcon] === "♥" || iconCard[contIcon] === "♦") {
+    document.querySelector(".iconUp").innerHTML = iconCard[contIcon];
+    document.querySelector(".iconDown").innerHTML = iconCard[contIcon];
+  }
+  document.querySelector(".iconUp").innerHTML = iconCard[contIcon];
+  document.querySelector(".iconDown").innerHTML = iconCard[contIcon];
+  contIcon++;
+  if (contIcon == 3) contIcon = 0;
+  contNum++;
+  if (contNum == 13) contNum = 0;
 };
 
-const numbersRandom = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
+const cambio = () => {
+  let itemNum = Math.floor(Math.random() * 12);
+  let itemIcon = Math.floor(Math.random() * 4);
+  document.querySelector(".numberCard").innerHTML = numbersCard[itemNum];
+  document.querySelector(".iconUp").innerHTML = iconCard[itemIcon];
+  document.querySelector(".iconDown").innerHTML = iconCard[itemIcon];
 };
-
-const iconsRandom = () => {
-  let icons = ["diamond", "spade", "heart", "club"];
-  let indexIcons = Math.floor(Math.random() * icons.length);
-};
+window.onload = inicio;
